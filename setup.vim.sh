b5a5dec253597f00e637ad710c719556f86e4af8
Sherlock W <wisdomnji@gmail.com>
2021-01-19 02:24:15 +0100

    updated vim configs

--- setup.vim.sh
+++ setup.vim.sh
@@ -2,9 +2,21 @@
 
 
 # install pathogen
-echo ">> Installing pathogen...."
+echo "[+] Installing pathogen...."
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-echo ">> Configuring pathogen..."
-echo -e "execute pathogen#infect()\nsyntax on\nfiltype plugin indent on\n" >> ~/.vimrc
+echo "- Configuring pathogen..."
+echo -e \
+"execute pathogen#infect()
+syntax on
+filetype plugin indent on
+set incsearch
+set number
+let g:airline#extensions#tabline#enabled = 1
+let g:airline#extensions#tabline#buffer_nr_show = 1" >> ~/.vimrc
+
+echo "[+] Installing Bundle: Airline..."
+cd ~/.vim/bundle
+git clone https://github.com/vim-airline/vim-airline
+git clone https://github.com/vim-airline/vim-airline-themes
